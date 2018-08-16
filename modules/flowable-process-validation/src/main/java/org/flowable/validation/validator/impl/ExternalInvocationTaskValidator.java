a6df38f48667d1575a0455aa9083d562ec6cc6ed
Gregory Loveridge <greg@relational.com.au>
2018-08-16 17:19:26 +0930

    * Commit last changes to this iteration before changing approach

--- modules/flowable-process-validation/src/main/java/org/flowable/validation/validator/impl/ExternalInvocationTaskValidator.java
+++ modules/flowable-process-validation/src/main/java/org/flowable/validation/validator/impl/ExternalInvocationTaskValidator.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,110 +14,153 @@ package org.flowable.validation.validator.impl;
 
 import java.util.List;
 
+import org.flowable.bpmn.model.ExtensionAttribute;
 import org.flowable.bpmn.model.FieldExtension;
 import org.flowable.bpmn.model.TaskWithFieldExtensions;
 import org.flowable.validation.ValidationError;
 import org.flowable.validation.validator.Problems;
 import org.flowable.validation.validator.ProcessLevelValidator;
 
-public abstract class ExternalInvocationTaskValidator extends ProcessLevelValidator {
+public abstract class ExternalInvocationTaskValidator extends ProcessLevelValidator
+{
 
-    protected void validateFieldDeclarationsForEmail(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors) {
-        boolean toDefined = false;
-        boolean textOrHtmlDefined = false;
+  protected void validateFieldDeclarationsForEmail(org.flowable.bpmn.model.Process process,
+      TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors)
+  {
+    boolean toDefined = false;
+    boolean textOrHtmlOrTemplateDefined = false;
 
-        for (FieldExtension fieldExtension : fieldExtensions) {
-            if (fieldExtension.getFieldName().equals("to")) {
-                toDefined = true;
-            }
-            if (fieldExtension.getFieldName().equals("html")) {
-                textOrHtmlDefined = true;
-            }
-            if (fieldExtension.getFieldName().equals("htmlVar")) {
-                textOrHtmlDefined = true;
-            }
-            if (fieldExtension.getFieldName().equals("text")) {
-                textOrHtmlDefined = true;
-            }
-            if (fieldExtension.getFieldName().equals("textVar")) {
-                textOrHtmlDefined = true;
-            }
-        }
-
-        if (!toDefined) {
-            addError(errors, Problems.MAIL_TASK_NO_RECIPIENT, process, task, "No recipient is defined on the mail activity");
-        }
-        if (!textOrHtmlDefined) {
-            addError(errors, Problems.MAIL_TASK_NO_CONTENT, process, task, "Text, html, textVar or htmlVar field should be provided");
-        }
+    for (FieldExtension fieldExtension : fieldExtensions)
+    {
+      if (fieldExtension.getFieldName().equals("to"))
+      {
+        toDefined = true;
+      }
+      if (fieldExtension.getFieldName().equals("html"))
+      {
+        textOrHtmlOrTemplateDefined = true;
+      }
+      if (fieldExtension.getFieldName().equals("htmlVar"))
+      {
+        textOrHtmlOrTemplateDefined = true;
+      }
+      if (fieldExtension.getFieldName().equals("text"))
+      {
+        textOrHtmlOrTemplateDefined = true;
+      }
+      if (fieldExtension.getFieldName().equals("textVar"))
+      {
+        textOrHtmlOrTemplateDefined = true;
+      }
+      List<ExtensionAttribute> extAttrs = task.getAttributes().get("mailtasktemplateid");
+      if (extAttrs != null && extAttrs.size() == 1 && extAttrs.get(0) != null && extAttrs.get(0).getValue() != null)
+      {
+        textOrHtmlOrTemplateDefined = true;
+      }
     }
 
-    protected void validateFieldDeclarationsForShell(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors) {
-        boolean shellCommandDefined = false;
+    if (!toDefined)
+    {
+      addError(errors, Problems.MAIL_TASK_NO_RECIPIENT, process, task, "No recipient is defined on the mail activity");
+    }
+    if (!textOrHtmlOrTemplateDefined)
+    {
+      addError(errors, Problems.MAIL_TASK_NO_CONTENT, process, task,
+          "Text, html, textVar, htmlVar field or the mailtasktemplateId attribute should be provided");
+    }
+  }
 
-        for (FieldExtension fieldExtension : fieldExtensions) {
-            String fieldName = fieldExtension.getFieldName();
-            String fieldValue = fieldExtension.getStringValue();
+  protected void validateFieldDeclarationsForShell(org.flowable.bpmn.model.Process process,
+      TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors)
+  {
+    boolean shellCommandDefined = false;
 
-            if (fieldName.equals("command")) {
-                shellCommandDefined = true;
-            }
+    for (FieldExtension fieldExtension : fieldExtensions)
+    {
+      String fieldName = fieldExtension.getFieldName();
+      String fieldValue = fieldExtension.getStringValue();
 
-            if ((fieldName.equals("wait") || fieldName.equals("redirectError") || fieldName.equals("cleanEnv")) && !fieldValue.toLowerCase().equals("true") && !fieldValue.toLowerCase().equals("false")) {
-                addError(errors, Problems.SHELL_TASK_INVALID_PARAM, process, task, "Undefined parameter value for shell field");
-            }
+      if (fieldName.equals("command"))
+      {
+        shellCommandDefined = true;
+      }
 
-        }
+      if ((fieldName.equals("wait") || fieldName.equals("redirectError") || fieldName.equals("cleanEnv")) && !fieldValue
+          .toLowerCase().equals("true") && !fieldValue.toLowerCase().equals("false"))
+      {
+        addError(errors, Problems.SHELL_TASK_INVALID_PARAM, process, task, "Undefined parameter value for shell field");
+      }
 
-        if (!shellCommandDefined) {
-            addError(errors, Problems.SHELL_TASK_NO_COMMAND, process, task, "No shell command is defined on the shell activity");
-        }
     }
 
-    protected void validateFieldDeclarationsForDmn(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors) {
-        boolean keyDefined = false;
+    if (!shellCommandDefined)
+    {
+      addError(errors, Problems.SHELL_TASK_NO_COMMAND, process, task,
+          "No shell command is defined on the shell activity");
+    }
+  }
 
-        for (FieldExtension fieldExtension : fieldExtensions) {
-            String fieldName = fieldExtension.getFieldName();
-            String fieldValue = fieldExtension.getStringValue();
+  protected void validateFieldDeclarationsForDmn(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task,
+      List<FieldExtension> fieldExtensions, List<ValidationError> errors)
+  {
+    boolean keyDefined = false;
 
-            if (fieldName.equals("decisionTableReferenceKey") && fieldValue != null && fieldValue.length() > 0) {
-                keyDefined = true;
-            }
-        }
+    for (FieldExtension fieldExtension : fieldExtensions)
+    {
+      String fieldName = fieldExtension.getFieldName();
+      String fieldValue = fieldExtension.getStringValue();
 
-        if (!keyDefined) {
-            addError(errors, Problems.DMN_TASK_NO_KEY, process, task, "No decision table reference key is defined on the dmn activity");
-        }
+      if (fieldName.equals("decisionTableReferenceKey") && fieldValue != null && fieldValue.length() > 0)
+      {
+        keyDefined = true;
+      }
     }
 
-    protected void validateFieldDeclarationsForHttp(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task, List<FieldExtension> fieldExtensions, List<ValidationError> errors) {
-        boolean requestMethodDefined = false;
-        boolean requestUrlDefined = false;
-
-        for (FieldExtension fieldExtension : fieldExtensions) {
-
-            String fieldName = fieldExtension.getFieldName();
-            String fieldValue = fieldExtension.getStringValue();
-            String fieldExpression = fieldExtension.getExpression();
-
-            if (fieldName.equals("requestMethod") && ((fieldValue != null && fieldValue.length() > 0) || (fieldExpression != null && fieldExpression.length() > 0))) {
-                requestMethodDefined = true;
-            }
-
-            if (fieldName.equals("requestUrl") && ((fieldValue != null && fieldValue.length() > 0) || (fieldExpression != null && fieldExpression.length() > 0))) {
-                requestUrlDefined = true;
-            }
-        }
-
-        if (!requestMethodDefined) {
-            addError(errors, Problems.HTTP_TASK_NO_REQUEST_METHOD, process, task, "No request method is defined on the http activity");
-        }
-
-        if (!requestUrlDefined) {
-            addError(errors, Problems.HTTP_TASK_NO_REQUEST_URL, process, task, "No request url is defined on the http activity");
-        }
-
+    if (!keyDefined)
+    {
+      addError(errors, Problems.DMN_TASK_NO_KEY, process, task,
+          "No decision table reference key is defined on the dmn activity");
     }
+  }
+
+  protected void validateFieldDeclarationsForHttp(org.flowable.bpmn.model.Process process, TaskWithFieldExtensions task,
+      List<FieldExtension> fieldExtensions, List<ValidationError> errors)
+  {
+    boolean requestMethodDefined = false;
+    boolean requestUrlDefined = false;
+
+    for (FieldExtension fieldExtension : fieldExtensions)
+    {
+
+      String fieldName = fieldExtension.getFieldName();
+      String fieldValue = fieldExtension.getStringValue();
+      String fieldExpression = fieldExtension.getExpression();
+
+      if (fieldName.equals("requestMethod") && ((fieldValue != null && fieldValue.length() > 0) || (
+          fieldExpression != null && fieldExpression.length() > 0)))
+      {
+        requestMethodDefined = true;
+      }
+
+      if (fieldName.equals("requestUrl") && ((fieldValue != null && fieldValue.length() > 0) || (fieldExpression != null
+          && fieldExpression.length() > 0)))
+      {
+        requestUrlDefined = true;
+      }
+    }
+
+    if (!requestMethodDefined)
+    {
+      addError(errors, Problems.HTTP_TASK_NO_REQUEST_METHOD, process, task,
+          "No request method is defined on the http activity");
+    }
+
+    if (!requestUrlDefined)
+    {
+      addError(errors, Problems.HTTP_TASK_NO_REQUEST_URL, process, task,
+          "No request url is defined on the http activity");
+    }
+
+  }
 
 }
